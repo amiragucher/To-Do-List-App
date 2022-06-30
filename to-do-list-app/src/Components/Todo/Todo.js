767f59f77eb9dc58c14471573aacb11eb0cb6fa2
amiragucher <amiragucher@gmail.com>
2022-06-30 13:00:32 +0100

    App working

---
+++ to-do-list-app/src/Components/Todo/Todo.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import TodoForm from "../ToDoForm/TodoForm";
+import { TiTimes } from "react-icons/ti";
+import { ImPencil } from "react-icons/im";
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? `todo-row complete` : `todo-row`}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+        <TiTimes onClick={() => removeTodo(todo.id)} className="delete-icon" />
+        <ImPencil
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className="edit-icon"
+        />
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
